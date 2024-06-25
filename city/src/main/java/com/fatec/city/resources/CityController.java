01b8c64a20f55bf660aa54e40130caac07fd748f
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-06-25 09:27:31 -0300

    Adiciona endpoint GET/cities

--- city/src/main/java/com/fatec/city/resources/CityController.java
+++ city/src/main/java/com/fatec/city/resources/CityController.java
@@ -3,11 +3,12 @@ package com.fatec.city.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fatec.city.entities.City;
+import com.fatec.city.dto.CityResponse;
 import com.fatec.city.services.CityServices;
 
 @RestController
@@ -18,7 +19,7 @@ public class CityController {
     private CityServices cityServices;
 
     @GetMapping
-    public List<City> getCities(){
-        return cityServices.getCities();
+    public ResponseEntity<List<CityResponse>> getCities(){
+        return ResponseEntity.ok(cityServices.getCities());
     }
 }
--- city/src/main/java/com/fatec/city/services/CityServices.java
+++ city/src/main/java/com/fatec/city/services/CityServices.java
@@ -1,11 +1,14 @@
 package com.fatec.city.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fatec.city.dto.CityResponse;
 import com.fatec.city.entities.City;
+import com.fatec.city.mapping.CityMapper;
 import com.fatec.city.repositories.CityRepository;
 
 @Service
@@ -14,7 +17,9 @@ public class CityServices {
     @Autowired
     private CityRepository cityRepository;
 
-    public List<City> getCities(){
-        return cityRepository.findAll();
+    public List<CityResponse> getCities(){
+        List <City> city = cityRepository.findAll();
+
+        return city.stream().map(s -> CityMapper.toDTO(s)).collect(Collectors.toList());
     }
 }
