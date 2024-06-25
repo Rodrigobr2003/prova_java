75f1b46aeb80bd6f58aa9fc2aac4119265e20d82
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-06-25 10:11:03 -0300

    Adiciona validações

--- city/src/main/java/com/fatec/city/dto/CityRequest.java
+++ city/src/main/java/com/fatec/city/dto/CityRequest.java
@@ -1,4 +1,22 @@
 package com.fatec.city.dto;
 
-public record CityRequest(String nome, String estado, Integer populacao, Integer pib) {    
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CityRequest(
+    @NotNull(message = "Nome da cidade não pode ser vazio")
+    @NotBlank(message = "Nome da cidade não pode ser branco")
+    String nome,
+
+    @NotNull(message = "Nome do estado não pode ser vazio")
+    @NotBlank(message = "Nome do estado não pode ser branco")
+    String estado,
+
+    @NotNull(message = "Populacao não pode ser vazia")
+    Integer populacao,
+
+    @NotNull(message = "PIB não pode ser vazio")
+    Integer pib
+){    
+    
 }
--- city/src/main/java/com/fatec/city/resources/CityController.java
+++ city/src/main/java/com/fatec/city/resources/CityController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,7 +35,7 @@ public class CityController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Void> update(@PathVariable int id, @RequestBody CityRequest city){
+    public ResponseEntity<Void> update(@PathVariable int id, @Validated @RequestBody CityRequest city){
         this.cityServices.update(id, city);
         return ResponseEntity.ok().build();
     }
